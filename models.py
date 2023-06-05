1670d6e7bd34ac00572b08f1db672a5c09eb0ea8
riathetechie@gmail.com <74742605+rg663@users.noreply.github.com>
2023-06-05 17:07:13 -0400

    app created

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+# from django.utils import timezone
+
+from coldfront.core.allocation.models import *
+from coldfront.core.project.models import *
+from coldfront.core.resource.models import *
+from coldfront.core.user.models import *
