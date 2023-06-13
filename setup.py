fb8eb1e6458618ac1ab350f20f354021a1f4daec
riathetechie@gmail.com <74742605+rg663@users.noreply.github.com>
2023-06-13 18:06:15 -0400

    added setup for pip

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='weeklyreportapp',
+    version='1.0',
+    url='https://github.com/rg663/weeklyreportapp',
+    packages=find_packages()
+)
