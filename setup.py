9c86722ef3d4a217fb38e27ee27edda7a8e4e0dc
riathetechie@gmail.com <74742605+rg663@users.noreply.github.com>
2023-06-13 18:25:01 -0400

    typo fix

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from weeklyreportapp import __version__
 
 setup(
     name='weeklyreportapp',
-    version=__version__,
     url='https://github.com/rg663/weeklyreportapp',
     packages=find_packages(),
 )
